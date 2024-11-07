3153e3189f3a2efe926af4d1a41eef7540236e9a
zayn705 <9emdesign@gmail.com>
2024-11-07 18:15:28 -0400

    Update Angular/MongoDB added Feature: Routes

--- Api/app.js
+++ Api/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-const Post = require("./models/post");
+const postRoutes = require("./routes/postRoutes");
 const {
   log,
 } = require("@angular-devkit/build-angular/src/builders/ssr-dev-server");
@@ -45,61 +45,6 @@ app.use((req, res, next) => {
   });
 }); */
 
-/* Here i post to the Mongo database  */
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((createdPost) => {
-    //console.log(result);
-    res.status(201).json({
-      message: "Post added succesfully to MongoDB!",
-      postId: createdPost._id,
-    });
-  });
-  //console.log(post);
-});
-/* Here i fetch frrom the Mongo database  */
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res
-      .status(200)
-      .json({ message: "Post fetched Successfully!", posts: documents });
-  });
-});
-
-/* Here i delete from the Mongo database */
-app.delete("/api/posts/:id", (req, res, next) => {
-  /* console.log(req.params.id);
-  res.status(200).json({ message: "Post has een deleted!" }); */
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post has een deleted!" });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    // console.log(result);
-    res.status(200).json({ message: "Update Post Successful!" });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not Found!" });
-    }
-  });
-});
 /* app.get("/api/posts", (req, res, next) => {
   const posts = [
     {
@@ -120,4 +65,5 @@ app.get("/api/posts/:id", (req, res, next) => {
   ];
   res.status(200).json({ message: "Post fetched Successfully!", posts: posts });
 }); */
+app.use(postRoutes);
 module.exports = app;
---
+++ Api/routes/postRoutes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
+
+/* Here i post to the Mongo database  */
+router.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((createdPost) => {
+    //console.log(result);
+    res.status(201).json({
+      message: "Post added succesfully to MongoDB!",
+      postId: createdPost._id,
+    });
+  });
+  //console.log(post);
+});
+/* Here i fetch frrom the Mongo database  */
+router.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res
+      .status(200)
+      .json({ message: "Post fetched Successfully!", posts: documents });
+  });
+});
+
+/* Here i delete from the Mongo database */
+router.delete("/api/posts/:id", (req, res, next) => {
+  /* console.log(req.params.id);
+  res.status(200).json({ message: "Post has een deleted!" }); */
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: "Post has een deleted!" });
+  });
+});
+
+router.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+  });
+  Post.updateOne({ _id: req.params.id }, post).then((result) => {
+    // console.log(result);
+    res.status(200).json({ message: "Update Post Successful!" });
+  });
+});
+
+router.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then((post) => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not Found!" });
+    }
+  });
+});
+
+module.exports = router;
